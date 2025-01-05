d0bad94c88d3f2a10073a689b46d3ee954155018
Simvolokov Pavel <psimvolokov@gmail.com>
2025-01-05 12:36:12 +0300

    add sending private link by stars payment

--- tg-server/index.js
+++ tg-server/index.js
@@ -9,7 +9,7 @@ const bot = new Bot("7882885644:AAFViuyV0uMLIbJEV2_1VnEQmC2nBiMMKKE");
 app.use(cors());
 app.use(bodyParser.json());
 
-app.post("/api/donate", (req, res) => {
+app.post("/api/donate", async (req, res) => {
   const userId = req.body.userId;
   const productName = req.body.productName || "Test Product";
   const amount = req.body.amount || 1;
@@ -28,6 +28,7 @@ app.post("/api/donate", (req, res) => {
     .then((invoice) => {
       console.log(userId);
       console.log(invoice);
+
       res.status(200).json({ invoice_link: invoice });
     })
     .catch((err) => {
@@ -36,24 +37,33 @@ app.post("/api/donate", (req, res) => {
     });
 });
 
+const sendMessageToUser = async () => {
+  const link = await bot.api.createChatInviteLink(-1001649327548, {
+    member_limit: 1,
+  });
+  console.log(link);
+  const message = `message: ${link.invite_link}`;
+  await bot.api.sendMessage(userId, message);
+};
+
 bot.on("pre_checkout_query", (ctx) => {
   return ctx.answerPreCheckoutQuery(true).catch(() => {
     console.error("answerPreCheckoutQuery failed");
   });
 });
 
-bot.on("message:successful_payment", (ctx) => {
+bot.on("message:successful_payment", async (ctx) => {
   if (!ctx.message || !ctx.message.successful_payment || !ctx.from) {
     return;
   }
+
+  await sendMessageToUser();
 
   const paymentInfo = ctx.message.successful_payment;
   console.log(paymentInfo);
   ctx
     .reply(
-      `Спасибо за вашу оплату! Вы успешно оплатили ${
-        paymentInfo.total_amount / 100
-      } ${paymentInfo.currency} за ${paymentInfo.invoice_payload}.`
+      `Спасибо за вашу оплату! Вы успешно оплатили ${paymentInfo.total_amount} ${paymentInfo.currency} за ${paymentInfo.invoice_payload}.`
     )
     .catch((err) => {
       console.error("Error sending message to user:", err);
